95c4520efe72e302bab8cea8d5ec851eb4dcbf20
akavl4d-ai <akavl4d@gmail.com>
2025-10-29 01:44:43 -0300

    Create firebase-config.js with Firebase settings
    Add Firebase configuration file with necessary keys.

---
+++ firebase - config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+// Copie este arquivo para o seu repositório como "firebase-config.js"
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDdQs-5oVTR6C2NGEJdPgzCHpywdzGdAGI",
+  authDomain: "dondokas.firebaseapp.com",
+  projectId: "dondokas",
+  storageBucket: "dondokas.firebasestorage.app",
+  messagingSenderId: "942792883888",
+  appId: "1:942792883888:web:47d118999f3b84fb534575"
+};
